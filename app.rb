bbb26572b8bfa158309b90d1668f8ba8e1031cdf
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-02-28 19:47:17 +0000

    Fixed Requirements, variable names

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'dotenv/load'
 require "http"
 
 get("/") do
+ 
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_KEY")}"
   @raw_response = HTTP.get(api_url)
   @raw_string = @raw_response.to_s
